9992a9260a9d8a2b533b6069b09f462f2b194e04
scienceclubmecs <scienceclubmecs@gmail.com>
2026-01-19 01:47:55 +0530

    Update Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -13,3 +13,19 @@ const Layout = ({ children }) => {
 };
 
 export default Layout;
+
+import GuideChatbot from "./GuideChatbot";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-bgDark text-white flex flex-col relative">
+      <NavBar />
+      <main className="flex-1 px-4 py-6 lg:px-8">{children}</main>
+      <footer className="border-t border-gray-700 text-xs text-gray-400 px-4 py-3">
+        Science & Tech Club • Built by Developers team
+      </footer>
+      <GuideChatbot />
+    </div>
+  );
+};
+
