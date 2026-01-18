64068b1fed53f3ba1180e43504170b5cdb3c1815
scienceclubmecs <scienceclubmecs@gmail.com>
2026-01-19 00:56:16 +0530

    Create enums.js

---
+++ server/src/models/enums.js
@@ -0,0 +1,23 @@
+export const ROLES = {
+  STUDENT: "student",
+  FACULTY: "faculty",
+  ADMIN: "admin",
+  COMMITTEE_CHAIR: "committee_chair",
+  COMMITTEE_VICE_CHAIR: "committee_vice_chair",
+  SECRETARY: "secretary",
+  VICE_SECRETARY: "vice_secretary",
+  EXECUTIVE_HEAD: "executive_head",
+  EXECUTIVE: "executive",
+  REPRESENTATIVE_HEAD: "representative_head",
+  REPRESENTATIVE: "representative",
+  DEVELOPER_HEAD: "developer_head",
+  DEVELOPER: "developer"
+};
+
+export const DEPARTMENTS = [
+  "CSE/AIML/CSD",
+  "IT/CME",
+  "CIVIL/MECH",
+  "ECE",
+  "EEE"
+];
