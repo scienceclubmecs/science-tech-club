a88045301ea077c57e9e846a8c90b968dd439a43
scienceclubmecs <scienceclubmecs@gmail.com>
2026-01-19 01:10:11 +0530

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: "#020202",
+        bgLight: "#ffffff",
+        accent: "#f5f5f5"
+      }
+    }
+  },
+  darkMode: "class",
+  plugins: []
+};
