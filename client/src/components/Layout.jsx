62ba94c1f37303c47b5e2f4c002f6842255913e3
scienceclubmecs <scienceclubmecs@gmail.com>
2026-01-19 15:16:22 +0530

    Update Layout.jsx

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -2,30 +2,14 @@ import NavBar from "./NavBar";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen bg-bgDark text-white flex flex-col">
+    <div className="min-h-screen bg-bgDark text-white flex flex-col relative">
       <NavBar />
       <main className="flex-1 px-4 py-6 lg:px-8">{children}</main>
       <footer className="border-t border-gray-700 text-xs text-gray-400 px-4 py-3">
-        Science & Tech Club • Built by Developers team
+        Science & Tech Club
       </footer>
     </div>
   );
 };
 
 export default Layout;
-
-import GuideChatbot from "./GuideChatbot";
-
-const Layout = ({ children }) => {
-  return (
-    <div className="min-h-screen bg-bgDark text-white flex flex-col relative">
-      <NavBar />
-      <main className="flex-1 px-4 py-6 lg:px-8">{children}</main>
-      <footer className="border-t border-gray-700 text-xs text-gray-400 px-4 py-3">
-        Science & Tech Club • Built by Developers team
-      </footer>
-      <GuideChatbot />
-    </div>
-  );
-};
-
