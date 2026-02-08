0a5836017c1e13065c92d7cb73f3ef26953a97a4
scienceclubmecs <scienceclubmecs@gmail.com>
2026-02-08 18:39:06 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
       }
     }
   }
