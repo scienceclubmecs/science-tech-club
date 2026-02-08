afcb992dc20a10006360a32e87ea9a243a7ed71b
scienceclubmecs <scienceclubmecs@gmail.com>
2026-02-08 22:48:51 +0530

    Update auth.js

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,19 +1,17 @@
 const jwt = require('jsonwebtoken');
 
-const auth = (req, res, next) => {
+module.exports = (req, res, next) => {
   try {
     const token = req.header('Authorization')?.replace('Bearer ', '');
     
     if (!token) {
-      return res.status(401).json({ message: 'No authentication token' });
+      return res.status(401).json({ message: 'No token provided' });
     }
-    
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || 'tDgSDMJFrSmZUPs8mAon7UDi59BYFdgtkL0ptwDzk5JtM5kFA7O1SjUAlzEzvUzdG50NhLrHk2aRHDOgPmR+Rg==');
     req.user = decoded;
     next();
   } catch (error) {
     res.status(401).json({ message: 'Invalid token' });
   }
 };
-
-module.exports = auth;
