d1844a032c5e9befe43d7f98aaf0905bd5b4ad5d
scienceclubmecs <scienceclubmecs@gmail.com>
2026-02-08 17:37:31 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,14 +1,15 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000/api'
+const API_URL = import.meta.env.VITE_API_URL || 'https://science-tech-club-iju0.onrender.com/api'
 
 const api = axios.create({
-  baseURL: API_BASE,
+  baseURL: API_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 })
 
+// Add token to requests
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
   if (token) {
